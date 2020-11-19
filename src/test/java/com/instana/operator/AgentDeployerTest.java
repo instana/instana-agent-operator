125c341ca18f3669009fb9b55d4bc5c0923b4819
Dahlia Bock <149529+dlbock@users.noreply.github.com>
2020-11-19 09:56:31 -0600

    Add labels to the Agent resources to help identify how the Agent is installed (#44)
    * The apiextensions.k8s.io/v1beta1 version of CustomResourceDefinition is deprecated and will no longer be served in v1.19. Use apiextensions.k8s.io/v1 instead.
    
    This is according to the 'Deprecations and Removals' section in: https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.16.md#deprecations-and-removals
    
    * Add app.kubernetes.io/managed-by and app.kubernetes.io/version labels to the Agent resources to help identify how the Agent is installed
    
    Agents installed by Helm have a similar labels for 'managed-by' and 'version'. These labels are later used to construct telemetry data to better understand how our customers are installing the Agent.
    
    Add `app.kubernetes.io/managed-by` and `app.kubernetes.io/version` labels to the Agent resources to help identify how the Agent is installed. The `app.kubernetes.io/managed-by` label can be added in the specific resource's YAML, but the `app.kubernetes.io/version` label has to be added dynamically from the `CustomResourceDefinition` just after the resource is loaded from its YAML.
    
    In order to use fabric8's Kubernetes test framework, we need to include the 'quarkus-test-kubernetes-client' dependency

--- src/test/java/com/instana/operator/AgentDeployerTest.java
+++ src/test/java/com/instana/operator/AgentDeployerTest.java
@@ -1,31 +1,56 @@
 package com.instana.operator;
 
+import static com.instana.operator.env.Environment.RELATED_IMAGE_INSTANA_AGENT;
+import static java.net.HttpURLConnection.HTTP_OK;
+import static java.util.Collections.emptyMap;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import com.google.common.collect.ImmutableMap;
 import com.instana.operator.customresource.InstanaAgent;
 import com.instana.operator.customresource.InstanaAgentSpec;
 import com.instana.operator.env.Environment;
+
 import io.fabric8.kubernetes.api.model.Container;
 import io.fabric8.kubernetes.api.model.EnvVar;
+import io.fabric8.kubernetes.api.model.apiextensions.CustomResourceDefinitionBuilder;
 import io.fabric8.kubernetes.api.model.apps.DaemonSet;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
-import org.junit.jupiter.api.Test;
+import io.fabric8.kubernetes.client.server.mock.KubernetesMockServer;
 
-import java.util.Collections;
+public class AgentDeployerTest {
 
-import static com.instana.operator.env.Environment.RELATED_IMAGE_INSTANA_AGENT;
-import static java.util.Collections.emptyMap;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.is;
+  private DefaultKubernetesClient client;
+  private KubernetesMockServer server;
 
-class AgentDeployerTest {
+  @BeforeEach
+  public void setup() {
+    server = new KubernetesMockServer();
+    server.start();
+    client = (DefaultKubernetesClient) server.createClient();
+  }
 
-  private final DefaultKubernetesClient client = new DefaultKubernetesClient();
+  @AfterEach
+  public void teardown() {
+    server.shutdown();
+  }
 
   @Test
   void daemonset_must_include_environment() {
     AgentDeployer deployer = new AgentDeployer();
+    deployer.setDefaultClient(client);
     deployer.setEnvironment(empty());
 
     InstanaAgentSpec agentSpec = new InstanaAgentSpec();
@@ -33,11 +58,11 @@ class AgentDeployerTest {
         .put("INSTANA_AGENT_MODE", "APM")
         .build());
 
-    InstanaAgent crd = new InstanaAgent();
-    crd.setSpec(agentSpec);
+    InstanaAgent customResource = new InstanaAgent();
+    customResource.setSpec(agentSpec);
 
     DaemonSet daemonSet = deployer.newDaemonSet(
-        crd,
+        customResource,
         client.inNamespace("instana-agent").apps().daemonSets());
 
     Container agentContainer = getAgentContainer(daemonSet);
@@ -49,16 +74,17 @@ class AgentDeployerTest {
   @Test
   void daemonset_must_include_specified_image() {
     AgentDeployer deployer = new AgentDeployer();
+    deployer.setDefaultClient(client);
     deployer.setEnvironment(empty());
 
     InstanaAgentSpec agentSpec = new InstanaAgentSpec();
     agentSpec.setAgentImage("other/image:some-tag");
 
-    InstanaAgent crd = new InstanaAgent();
-    crd.setSpec(agentSpec);
+    InstanaAgent customResource = new InstanaAgent();
+    customResource.setSpec(agentSpec);
 
     DaemonSet daemonSet = deployer.newDaemonSet(
-        crd,
+        customResource,
         client.inNamespace("instana-agent").apps().daemonSets());
 
     Container agentContainer = getAgentContainer(daemonSet);
@@ -70,18 +96,78 @@ class AgentDeployerTest {
   @Test
   void daemonset_must_include_image_from_csv_if_specified() {
     AgentDeployer deployer = new AgentDeployer();
+    deployer.setDefaultClient(client);
     deployer.setEnvironment(singleVar(RELATED_IMAGE_INSTANA_AGENT, "other/image:some-tag"));
 
-    InstanaAgent crd = new InstanaAgent();
-    crd.setSpec(new InstanaAgentSpec());
+    InstanaAgent customResource = new InstanaAgent();
+    customResource.setSpec(new InstanaAgentSpec());
 
     DaemonSet daemonSet = deployer.newDaemonSet(
-        crd,
+        customResource,
         client.inNamespace("instana-agent").apps().daemonSets());
 
     Container agentContainer = getAgentContainer(daemonSet);
 
     assertThat(agentContainer.getImage(), is("other/image:some-tag"));
+  }
+
+  @Test
+  public void daemonset_must_include_version_label_if_specified_on_crd() {
+    server
+        .expect()
+        .get()
+        .withPath("/apis/apiextensions.k8s.io/v1beta1/customresourcedefinitions/agents.instana.io")
+        .andReturn(HTTP_OK, new CustomResourceDefinitionBuilder()
+                  .withNewMetadata()
+                  .withName("agents.instana.io")
+                  .withLabels(ImmutableMap.of("app.kubernetes.io/version", "0.3.8"))
+                  .endMetadata()
+                  .build())
+        .always();
+
+    AgentDeployer deployer = new AgentDeployer();
+    deployer.setDefaultClient(client);
+    deployer.setEnvironment(empty());
+
+    InstanaAgent customResource = new InstanaAgent();
+    customResource.setSpec(new InstanaAgentSpec());
+
+    DaemonSet daemonSet = deployer.newDaemonSet(
+        customResource,
+        client.inNamespace("instana-agent").apps().daemonSets());
+
+    Map<String, String> labels = daemonSet.getMetadata().getLabels();
+    assertThat(labels, hasEntry(is("app.kubernetes.io/managed-by"), is("instana-agent-operator")));
+    assertThat(labels, hasEntry(is("app.kubernetes.io/version"), is("0.3.8")));
+  }
+
+  @Test
+  public void daemonset_must_not_include_version_label_if_not_specified_on_crd() {
+    server
+        .expect()
+        .get()
+        .withPath("/apis/apiextensions.k8s.io/v1beta1/customresourcedefinitions/agents.instana.io")
+        .andReturn(HTTP_OK, new CustomResourceDefinitionBuilder()
+            .withNewMetadata()
+            .withName("agents.instana.io")
+            .endMetadata()
+            .build())
+        .always();
+
+    AgentDeployer deployer = new AgentDeployer();
+    deployer.setDefaultClient(client);
+    deployer.setEnvironment(empty());
+
+    InstanaAgent customResource = new InstanaAgent();
+    customResource.setSpec(new InstanaAgentSpec());
+
+    DaemonSet daemonSet = deployer.newDaemonSet(
+        customResource,
+        client.inNamespace("instana-agent").apps().daemonSets());
+
+    Map<String, String> labels = daemonSet.getMetadata().getLabels();
+    assertThat(labels, hasEntry(is("app.kubernetes.io/managed-by"), is("instana-agent-operator")));
+    assertThat(labels, not(hasKey("app.kubernetes.io/version")));
   }
 
   private Container getAgentContainer(DaemonSet daemonSet) {
@@ -95,5 +181,4 @@ class AgentDeployerTest {
   private Environment singleVar(String key, String value) {
     return Environment.fromMap(Collections.singletonMap(key, value));
   }
-
 }
