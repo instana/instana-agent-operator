9dcb5559d9508fcec806d67593a77904cb38b15a
yousefabdelhamid <yousef.abdelhamid@instana.com>
2021-04-16 07:23:06 +0200

    set agent's crd specs

--- api/v1beta1/instanaagent_types.go
+++ api/v1beta1/instanaagent_types.go
@@ -18,41 +18,73 @@ package v1beta1
 
 import (
 	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// ImageSpec describes an image name and tag to use
-type ImageSpec struct {
-	//Docker image name to use
-	Name string `json:"name"`
+// // ImageSpec describes an image name and tag to use
+// type ImageSpec struct {
+// 	//Docker image name to use
+// 	Name string `json:"name"`
 
-	//Docker image tag to use
-	Tag string `json:"tag"`
-}
+// 	//Docker image tag to use
+// 	Tag string `json:"tag"`
+// }
 
-//ConfigSpec describes the config template
-type ConfigSpec struct {
-	//Number of replicas, will default to 1
-	Replicas *int32 `json:"replicas,omitempty"`
+// //ConfigSpec describes the config template
+// type ConfigSpec struct {
+// 	//Number of replicas, will default to 1
+// 	Replicas *int32 `json:"replicas,omitempty"`
 
-	//Docker image to use
-	Image ImageSpec `json:"image"`
+// 	//Docker image to use
+// 	Image ImageSpec `json:"image"`
 
-	//Port, will default to 8000
-	Port *int32 `json:"port,omitempty"`
+// 	//Port, will default to 8000
+// 	Port *int32 `json:"port,omitempty"`
 
-	//Memory to allocate
-	Memory resource.Quantity `json:"memory"`
+// 	//Memory to allocate
+// 	Memory resource.Quantity `json:"memory"`
 
-	// Node group to have affinity toward
-	NodeAffinityLabel string `json:"nodeAffinityLabel,omitempty"`
+// 	// Node group to have affinity toward
+// 	NodeAffinityLabel string `json:"nodeAffinityLabel,omitempty"`
+// }
+
+// InstanaAgentSpec defines the desired state of the Instana Agent
+// type InstanaAgentSpec struct {
+// 	//Config spec
+// 	Config ConfigSpec `json:"config"`
+
+// }
+
+type InstanaAgentEndpoint struct {
+	Host string `json:"host,omitempty"`
+	Port string `json:"port,omitempty"`
 }
 
 // InstanaAgentSpec defines the desired state of the Instana Agent
+// +k8s:openapi-gen=true
 type InstanaAgentSpec struct {
-	//Config spec
-	Config ConfigSpec `json:"config"`
+	// Optional: Set the zone of the host
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors.x-descriptors="urn:alm:descriptor:com.tectonic.ui:text"
+	ZoneName string `json:"zone.name,omitempty"`
+
+	// your Instana agent key
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors.displayName="Agent Key"
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors.x-descriptors="urn:alm:descriptor:io.kubernetes:Secret"
+	Key string `json:"key,omitempty"`
+
+	// set agent's endpoint
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors.displayName="Agent Endpoint"
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors.x-descriptors="urn:alm:descriptor:com.tectonic.ui:advanced,urn:alm:descriptor:com.tectonic.ui:text"
+	Endpoint *InstanaAgentEndpoint `json:"endpoint,omitempty"`
+
+	// Set environment vars
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors.displayName="Agent environment variable"
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors.x-descriptors="urn:alm:descriptor:com.tectonic.ui:advanced,urn:alm:descriptor:com.tectonic.ui:text"
+	Env string `json:"env,omitempty"`
 }
 
 // InstanaAgentStatus defines the observed state of InstanaAgent
