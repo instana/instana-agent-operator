0e9c4abf514e1d4be0db20c28a5b30c2ccfd5532
ZachRobinson <zachrobinson@ibm.com>
2023-03-04 15:05:52 -0600

    label injection
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/transformations/transformations.go
+++ pkg/transformations/transformations.go
@@ -1,1 +1,20 @@
 package transformations
+
+import (
+	"os"
+
+	"github.com/instana/instana-agent-operator/pkg/optional"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func AddCommonLabels(obj client.Object) {
+	version := optional.Of(os.Getenv("OPERATOR_VERSION")).GetOrElse("v0.0.0")
+
+	labels := optional.Of(obj.GetLabels()).GetOrElse(make(map[string]string, 3))
+
+	labels["app.kubernetes.io/name"] = "instana-agent"
+	labels["app.kubernetes.io/instance"] = "instana-agent"
+	labels["app.kubernetes.io/version"] = version
+
+	obj.SetLabels(labels)
+}
