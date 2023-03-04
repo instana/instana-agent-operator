1a8dd57fd183a66e38251f2c403a194d30eb4b38
ZachRobinson <zachrobinson@ibm.com>
2023-03-04 11:13:26 -0600

    TODO
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/list/list.go
+++ pkg/list/list.go
@@ -2,7 +2,7 @@ package list
 
 type list[T any] struct {
 	raw []T
-}
+} // TODO: switch to collection use map as backer then add constructor for map and for list where key is the position
 
 // TODO: filter, map, forEach, add(...)
 
