42ed39e5a5c2542b3b5a18e3b93cc6b3df3f738f
ZachRobinson <zachrobinson@ibm.com>
2023-03-04 17:15:23 -0600

    TODO
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/transformations/transformations.go
+++ pkg/transformations/transformations.go
@@ -7,8 +7,13 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+var (
+	version = optional.Of(os.Getenv("OPERATOR_VERSION")).GetOrElse("v0.0.0")
+)
+
+// TODO: interface this for mocking
+
 func AddCommonLabels(obj client.Object) {
-	version := optional.Of(os.Getenv("OPERATOR_VERSION")).GetOrElse("v0.0.0")
 
 	labels := optional.Of(obj.GetLabels()).GetOrElse(make(map[string]string, 3))
 
