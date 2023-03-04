bf8515f22656a336f49947036b35c35aa37f024b
ZachRobinson <zachrobinson@ibm.com>
2023-03-04 12:34:03 -0600

    TODO
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/list/list.go
+++ pkg/list/list.go
@@ -1,9 +1,4 @@
 package list
 
-type list[T any] struct {
-	raw []T
-} // TODO: switch to collection use map as backer then add constructor for map and for list where key is the position
-
-// TODO: filter, map, forEach, add(...)
-
+// TODO: combineAll(...), filter, map
 // TODO: other todo, owned resources, exponential backoff config, general transformers interface + implement (common labels + owner refs), apply all function, basic controller tasks, then status later on
