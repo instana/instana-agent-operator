af5939765a43f40ef0585088a7bc8e261df7ef45
ZachRobinson <zachrobinson@ibm.com>
2023-03-05 12:07:53 -0600

    tests fix
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/k8s/object/transformations/transformations_test.go
+++ pkg/k8s/object/transformations/transformations_test.go
@@ -2,23 +2,27 @@ package transformations
 
 import (
 	instanav1 "github.com/instana/instana-agent-operator/api/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestAddCommonLabels(t *testing.T) {
 	t.Run("with_empty_labels_initially", func(t *testing.T) {
 		obj := v1.ConfigMap{}
-		NewTransformations(&instanav1.InstanaAgent{}).AddCommonLabels(&obj)
+		NewTransformations(&instanav1.InstanaAgent{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "asdf",
+			},
+		}).AddCommonLabels(&obj)
 
 		assertions := require.New(t)
 
 		assertions.Equal(map[string]string{
 			"app.kubernetes.io/name":     "instana-agent",
-			"app.kubernetes.io/instance": "instana-agent",
+			"app.kubernetes.io/instance": "asdf",
 			"app.kubernetes.io/version":  "v0.0.0",
 		}, obj.GetLabels())
 	})
@@ -32,7 +36,11 @@ func TestAddCommonLabels(t *testing.T) {
 			},
 		}
 
-		NewTransformations(&instanav1.InstanaAgent{}).AddCommonLabels(&obj)
+		NewTransformations(&instanav1.InstanaAgent{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "foo",
+			},
+		}).AddCommonLabels(&obj)
 
 		assertions := require.New(t)
 
@@ -40,7 +48,7 @@ func TestAddCommonLabels(t *testing.T) {
 			"foo":                        "bar",
 			"hello":                      "world",
 			"app.kubernetes.io/name":     "instana-agent",
-			"app.kubernetes.io/instance": "instana-agent",
+			"app.kubernetes.io/instance": "foo",
 			"app.kubernetes.io/version":  "v0.0.0",
 		}, obj.GetLabels())
 	})
