b8cc3ed238d6edfe2958bcfb186068557aa28bf5
ZachRobinson <zachrobinson@ibm.com>
2023-03-08 11:36:33 -0600

    WIP daemonset
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

---
+++ pkg/k8s/object/builders/helpers/helpers.go
@@ -0,0 +1,21 @@
+package helpers
+
+import (
+	v1 "github.com/instana/instana-agent-operator/api/v1"
+	"github.com/instana/instana-agent-operator/pkg/optional"
+)
+
+func serviceAccountNameDefault(agent *v1.InstanaAgent) string {
+	switch agent.Spec.ServiceAccountSpec.Create.Create {
+	case true:
+		return agent.Name
+	default:
+		return "default"
+	}
+}
+
+func ServiceAccountName(agent *v1.InstanaAgent) string {
+	return optional.Of(agent.Spec.ServiceAccountSpec.Name.Name).GetOrElse(serviceAccountNameDefault(agent))
+}
+
+// TODO: test
