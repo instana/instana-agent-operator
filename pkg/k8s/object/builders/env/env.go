f14c51c5e96b2d46bca4c58d37c95b698fdd10b3
ZachRobinson <zachrobinson@ibm.com>
2023-03-20 09:17:29 -0500

    general env set from values
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/k8s/object/builders/env/env.go
+++ pkg/k8s/object/builders/env/env.go
@@ -1,10 +1,36 @@
 package env
 
 import (
+	"fmt"
+
 	"github.com/instana/instana-agent-operator/pkg/optional"
 	corev1 "k8s.io/api/core/v1"
 )
 
 type EnvBuilder interface {
 	Build() optional.Optional[corev1.EnvVar]
+}
+
+type fromFieldIfSet[T any] struct {
+	name          string
+	providedValue optional.Optional[T]
+}
+
+func fromField[T any](name string, val T) EnvBuilder {
+	return &fromFieldIfSet[T]{
+		name:          name,
+		providedValue: optional.Of(val),
+	}
+}
+
+func (f *fromFieldIfSet[T]) Build() optional.Optional[corev1.EnvVar] {
+	switch f.providedValue.IsEmpty() {
+	case true:
+		return optional.Empty[corev1.EnvVar]()
+	default:
+		return optional.Of(corev1.EnvVar{
+			Name:  f.name,
+			Value: fmt.Sprintf("%v", f.providedValue.Get()),
+		})
+	}
 }
