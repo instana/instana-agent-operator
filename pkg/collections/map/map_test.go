4a19677d55a8166709bcedc96c9a545bded2e518
ZachRobinson <zachrobinson@ibm.com>
2023-03-23 12:07:58 -0500

    map to list mapper
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

---
+++ pkg/collections/map/map_test.go
@@ -0,0 +1,24 @@
+package _map
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMapConverter_ToList(t *testing.T) {
+	assertions := require.New(t)
+
+	in := map[string]string{
+		"foo":       "bar",
+		"hello":     "world",
+		"something": "else",
+	}
+
+	actual := NewMapConverter[string, string, string]().ToList(in, func(key string, val string) string {
+		return fmt.Sprintf("%s: %s", key, val)
+	})
+
+	assertions.Equal([]string{"foo: bar", "hello: world", "something: else"}, actual)
+}
