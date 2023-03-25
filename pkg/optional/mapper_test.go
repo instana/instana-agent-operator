043d34e634f2ed08a73d67950adb54d11402ae05
ZachRobinson <zachrobinson@ibm.com>
2023-03-25 12:24:55 -0500

    better non-empty test
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/optional/mapper_test.go
+++ pkg/optional/mapper_test.go
@@ -7,23 +7,23 @@ import (
 )
 
 func TestAllNonEmpty(t *testing.T) {
-	mpr := NewNonEmptyOptionalMapper[bool]()
+	mpr := NewNonEmptyOptionalMapper[int]()
 
-	in := []Optional[bool]{
-		Empty[bool](),
-		Of(true),
-		Of(false),
-		Of(true),
-		Of(true),
-		Empty[bool](),
-		Empty[bool](),
-		Empty[bool](),
-		Of(false),
-		Of(true),
+	in := []Optional[int]{
+		Empty[int](),
+		Of(1),
+		Of(0),
+		Of(2),
+		Of(3),
+		Empty[int](),
+		Empty[int](),
+		Empty[int](),
+		Of(4),
+		Of(5),
 	}
 	out := mpr.AllNonEmpty(in)
 
 	assertions := require.New(t)
 
-	assertions.Equal([]bool{true, true, true, true}, out)
+	assertions.Equal([]int{1, 2, 3, 4, 5}, out)
 }
