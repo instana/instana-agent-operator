a49d7c919692f76ff9c255f5506ab34f127d9875
ZachRobinson <zachrobinson@ibm.com>
2023-03-28 09:53:26 -0500

    result methods
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/result/result.go
+++ pkg/result/result.go
@@ -1,6 +1,9 @@
 package result
 
-import "github.com/instana/instana-agent-operator/pkg/optional"
+import (
+	"errors"
+	"github.com/instana/instana-agent-operator/pkg/optional"
+)
 
 type Result[T any] interface {
 	IsSuccess() bool
@@ -16,5 +19,51 @@ type result[T any] struct {
 	res T
 	err error
 }
+
+func (r *result[T]) IsSuccess() bool {
+	return errors.Is(r.err, nil)
+}
+
+func (r *result[T]) IsFailure() bool {
+	return !r.IsSuccess()
+}
+
+func (r *result[T]) Get() (T, error) {
+	return r.res, r.err
+}
+
+func (r *result[T]) ToOptional() optional.Optional[T] {
+	switch r.IsSuccess() {
+	case true:
+		return optional.Of(r.res)
+	default:
+		return optional.Empty[T]()
+	}
+}
+
+func (r *result[T]) OnSuccess(do func(res T)) Result[T] {
+	if r.IsSuccess() {
+		do(r.res)
+	}
+	return r
+}
+
+func (r *result[T]) OnFailure(do func(err error)) Result[T] {
+	if r.IsFailure() {
+		do(r.err)
+	}
+	return r
+}
+
+func (r *result[T]) Recover(do func(err error) Result[T]) Result[T] {
+	switch r.IsFailure() {
+	case true:
+		return do(r.err)
+	default:
+		return r
+	}
+}
+
+// TODO: Tests
 
 // TODO: Of, OfFunction, ofResult, ofError, Map
