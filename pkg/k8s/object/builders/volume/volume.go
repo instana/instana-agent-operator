f793b5be1ad72abeaea91f49f0b87baa9306e74a
ZachRobinson <zachrobinson@ibm.com>
2023-03-29 13:11:40 -0500

    host volume literal
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

---
+++ pkg/k8s/object/builders/volume/volume.go
@@ -0,0 +1,30 @@
+package volume
+
+import (
+	"github.com/instana/instana-agent-operator/pkg/optional"
+	corev1 "k8s.io/api/core/v1"
+)
+
+type fromHostLiteralParams struct {
+	name string
+	path string
+	*corev1.MountPropagationMode
+}
+
+func fromHostLiteral(params *fromHostLiteralParams) (optional.Builder[corev1.Volume], optional.Builder[corev1.VolumeMount]) {
+	return optional.BuilderFromLiteral(corev1.Volume{
+			Name: params.name,
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: params.path,
+				},
+			},
+		}),
+		optional.BuilderFromLiteral(corev1.VolumeMount{
+			Name:             params.name,
+			MountPath:        params.path,
+			MountPropagation: params.MountPropagationMode,
+		})
+}
+
+// TODO
