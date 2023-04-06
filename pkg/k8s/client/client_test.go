48f44bf641063eaac76f2209812663df6b32b0fb
ZachRobinson <zachrobinson@ibm.com>
2023-04-06 09:54:36 -0500

    get as result test
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/k8s/client/client_test.go
+++ pkg/k8s/client/client_test.go
@@ -2,21 +2,26 @@ package client
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
 	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/instana/instana-agent-operator/pkg/result"
 )
 
-func TestApply(t *testing.T) {
+func TestInstanaAgentClient_Apply(t *testing.T) {
 	ctrl := gomock.NewController(t)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	cm := v1.ConfigMap{}
+	cm := corev1.ConfigMap{}
 	opts := []k8sclient.PatchOption{k8sclient.DryRunAll}
 	expectedErr := errors.New("awojsgeoisegoijsdg")
 
@@ -43,4 +48,44 @@ func TestApply(t *testing.T) {
 
 	assertions.Same(&cm, actualVal)
 	assertions.Equal(expectedErr, actualErr)
+}
+
+func TestInstanaAgentClient_GetAsResult(t *testing.T) {
+	assertions := require.New(t)
+	ctrl := gomock.NewController(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	key := types.NamespacedName{
+		Namespace: "adsf",
+		Name:      "rasdgf",
+	}
+	obj := &unstructured.Unstructured{}
+	opts := &k8sclient.GetOptions{
+		Raw: &metav1.GetOptions{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "reoisoijd",
+				APIVersion: "erifoijsd",
+			},
+			ResourceVersion: "adsfadsf",
+		},
+	}
+
+	mockK8sClient := NewMockClient(ctrl)
+	mockK8sClient.EXPECT().Get(
+		gomock.Eq(ctx),
+		gomock.Eq(key),
+		gomock.Eq(obj),
+		gomock.Eq(opts),
+		gomock.Eq(opts),
+	).Return(errors.New("foo"))
+
+	mockTransformations := NewMockTransformations(ctrl)
+	client := instanaAgentClient{
+		Client:          mockK8sClient,
+		Transformations: mockTransformations,
+	}
+
+	actual := client.GetAsResult(ctx, key, obj, opts, opts)
+	assertions.Equal(result.Of[k8sclient.Object](obj, errors.New("foo")), actual)
 }
