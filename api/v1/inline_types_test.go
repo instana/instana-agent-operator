7a950821ae3c34eae212b8aeeba43214b394cbdc
ZachRobinson <zachrobinson@ibm.com>
2023-04-12 12:52:57 -0500

    test cleanup
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- api/v1/inline_types_test.go
+++ api/v1/inline_types_test.go
@@ -1,52 +1,58 @@
 package v1
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestImageSpec_Image(t *testing.T) {
-	t.Run("with digest", func(t *testing.T) {
-		assertions := require.New(t)
+	for _, test := range []struct {
+		name string
+		ImageSpec
+		expected string
+	}{
+		{
+			name: "with_digest",
+			ImageSpec: ImageSpec{
+				Name:   "icr.io/instana/instana-agent-operator",
+				Digest: "sha256:61417f330b2eb7eff88ccb9812921b65a31bf350fe9efdcb6663a29759c47fe4",
+			},
+			expected: "icr.io/instana/instana-agent-operator@sha256:61417f330b2eb7eff88ccb9812921b65a31bf350fe9efdcb6663a29759c47fe4",
+		},
+		{
+			name: "with_digest_and_tag",
+			ImageSpec: ImageSpec{
+				Name:   "icr.io/instana/instana-agent-operator",
+				Digest: "sha256:61417f330b2eb7eff88ccb9812921b65a31bf350fe9efdcb6663a29759c47fe4",
+				Tag:    "2.0.10",
+			},
+			expected: "icr.io/instana/instana-agent-operator@sha256:61417f330b2eb7eff88ccb9812921b65a31bf350fe9efdcb6663a29759c47fe4",
+		},
+		{
+			name: "with_tag",
+			ImageSpec: ImageSpec{
+				Name: "icr.io/instana/instana-agent-operator",
+				Tag:  "2.0.10",
+			},
+			expected: "icr.io/instana/instana-agent-operator:2.0.10",
+		},
+		{
+			name: "with_name_only",
+			ImageSpec: ImageSpec{
+				Name: "icr.io/instana/instana-agent-operator:2.0.10",
+			},
+			expected: "icr.io/instana/instana-agent-operator:2.0.10",
+		},
+	} {
+		t.Run(
+			test.name, func(t *testing.T) {
+				assertions := require.New(t)
 
-		i := ImageSpec{
-			Name:   "icr.io/instana/instana-agent-operator",
-			Digest: "sha256:61417f330b2eb7eff88ccb9812921b65a31bf350fe9efdcb6663a29759c47fe4",
-		}
+				actual := test.ImageSpec.Image()
 
-		assertions.Equal("icr.io/instana/instana-agent-operator@sha256:61417f330b2eb7eff88ccb9812921b65a31bf350fe9efdcb6663a29759c47fe4", i.Image())
-	})
-
-	t.Run("with digest and tag", func(t *testing.T) {
-		assertions := require.New(t)
-
-		i := ImageSpec{
-			Name:   "icr.io/instana/instana-agent-operator",
-			Digest: "sha256:61417f330b2eb7eff88ccb9812921b65a31bf350fe9efdcb6663a29759c47fe4",
-			Tag:    "2.0.10",
-		}
-
-		assertions.Equal("icr.io/instana/instana-agent-operator@sha256:61417f330b2eb7eff88ccb9812921b65a31bf350fe9efdcb6663a29759c47fe4", i.Image())
-	})
-
-	t.Run("with tag", func(t *testing.T) {
-		assertions := require.New(t)
-
-		i := ImageSpec{
-			Name: "icr.io/instana/instana-agent-operator",
-			Tag:  "2.0.10",
-		}
-
-		assertions.Equal("icr.io/instana/instana-agent-operator:2.0.10", i.Image())
-	})
-
-	t.Run("with name only", func(t *testing.T) {
-		assertions := require.New(t)
-
-		i := ImageSpec{
-			Name: "icr.io/instana/instana-agent-operator:2.0.10",
-		}
-
-		assertions.Equal("icr.io/instana/instana-agent-operator:2.0.10", i.Image())
-	})
+				assertions.Equal(test.expected, actual)
+			},
+		)
+	}
 }
