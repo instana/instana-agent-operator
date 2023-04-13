3963031873318b342459035a3068089699fdb470
ZachRobinson <zachrobinson@ibm.com>
2023-04-13 12:05:05 -0500

    cleanup TestDerefOrDefault
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/pointer/pointer_test.go
+++ pkg/pointer/pointer_test.go
@@ -44,24 +44,33 @@ func TestDerefOrEmpty(t *testing.T) {
 }
 
 func TestDerefOrDefault(t *testing.T) {
-	t.Run(
-		"non_nil_pointer_given", func(t *testing.T) {
-			assertions := require.New(t)
-
-			actual := DerefOrDefault(To(5), 10)
-
-			assertions.Equal(5, actual)
+	for _, tt := range []struct {
+		name   string
+		input  *int
+		defVal int
+		want   int
+	}{
+		{
+			name:   "non_nil_pointer_given",
+			input:  To(5),
+			defVal: 10,
+			want:   5,
 		},
-	)
-	t.Run(
-		"nil_pointer_given", func(t *testing.T) {
-			assertions := require.New(t)
-
-			actual := DerefOrDefault(nil, 10)
-
-			assertions.Equal(10, actual)
+		{
+			name:   "nil_pointer_given",
+			input:  nil,
+			defVal: 10,
+			want:   10,
 		},
-	)
+	} {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				assertions := require.New(t)
+				actual := DerefOrDefault(tt.input, tt.defVal)
+				assertions.Equal(tt.want, actual)
+			},
+		)
+	}
 }
 
 func TestDerefOrElse(t *testing.T) {
