e2d80357caadf4df7bb06508ec071853645fc4b0
ZachRobinson <zachrobinson@ibm.com>
2023-04-13 11:44:30 -0500

    TestDerefOrEmpty
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/pointer/pointer_test.go
+++ pkg/pointer/pointer_test.go
@@ -15,24 +15,32 @@ func TestTo(t *testing.T) {
 }
 
 func TestDerefOrEmpty(t *testing.T) {
-	t.Run(
-		"non_nil_pointer_given", func(t *testing.T) {
-			assertions := require.New(t)
-
-			actual := DerefOrEmpty(To(10))
-
-			assertions.Equal(10, actual)
+	for _, tc := range []struct {
+		name   string
+		input  *int
+		output int
+	}{
+		{
+			name:   "non_nil_pointer_given",
+			input:  To(10),
+			output: 10,
 		},
-	)
-	t.Run(
-		"nil_pointer_given", func(t *testing.T) {
-			assertions := require.New(t)
-
-			actual := DerefOrEmpty[int](nil)
-
-			assertions.Equal(0, actual)
+		{
+			name:   "nil_pointer_given",
+			input:  nil,
+			output: 0,
 		},
-	)
+	} {
+		t.Run(
+			tc.name, func(t *testing.T) {
+				assertions := require.New(t)
+
+				actual := DerefOrEmpty(tc.input)
+
+				assertions.Equal(tc.output, actual)
+			},
+		)
+	}
 }
 
 func TestDerefOrDefault(t *testing.T) {
