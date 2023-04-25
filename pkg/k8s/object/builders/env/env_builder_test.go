94a49e7aa67fa6bb2c897e78b4e212d99352d516
ZachRobinson <zachrobinson@ibm.com>
2023-04-25 09:04:30 -0500

    basic test for Build()
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/k8s/object/builders/env/env_builder_test.go
+++ pkg/k8s/object/builders/env/env_builder_test.go
@@ -5,7 +5,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 
+	instanav1 "github.com/instana/instana-agent-operator/api/v1"
 	"github.com/instana/instana-agent-operator/pkg/collections/list"
 )
 
@@ -65,4 +67,52 @@ func TestEnvBuilder_getBuilder(t *testing.T) {
 			)
 		},
 	)
+}
+
+func TestEnvBuilder_Build(t *testing.T) {
+	assertions := require.New(t)
+
+	expected := []corev1.EnvVar{
+		{
+			Name:  "MY_VAR1",
+			Value: "MY_VAL1",
+		},
+		{
+			Name:  "MY_VAR2",
+			Value: "MY_VAL2",
+		},
+		{
+			Name: "INSTANA_AGENT_POD_NAME",
+			ValueFrom: &corev1.EnvVarSource{
+				FieldRef: &corev1.ObjectFieldSelector{
+					FieldPath: "metadata.name",
+				},
+			},
+		},
+		{
+			Name: "POD_IP",
+			ValueFrom: &corev1.EnvVarSource{
+				FieldRef: &corev1.ObjectFieldSelector{
+					FieldPath: "status.podIP",
+				},
+			},
+		},
+	}
+
+	eb := NewEnvBuilder(
+		&instanav1.InstanaAgent{
+			Spec: instanav1.InstanaAgentSpec{
+				Agent: instanav1.BaseAgentSpec{
+					Env: map[string]string{
+						"MY_VAR1": "MY_VAL1",
+						"MY_VAR2": "MY_VAL2",
+					},
+				},
+			},
+		},
+	)
+
+	actual := eb.Build(PodNameEnv, PodIPEnv)
+
+	assertions.Equal(expected, actual)
 }
