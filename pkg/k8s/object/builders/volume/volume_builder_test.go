2a60f18751033499663bfbb512f2a4723e7ae596
ZachRobinson <zachrobinson@ibm.com>
2023-05-18 13:15:08 -0500

    some volume_builder_test.go
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

---
+++ pkg/k8s/object/builders/volume/volume_builder_test.go
@@ -0,0 +1,102 @@
+package volume
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	instanav1 "github.com/instana/instana-agent-operator/api/v1"
+	"github.com/instana/instana-agent-operator/pkg/collections/list"
+)
+
+const numDefinedVolumes = 13
+
+func rangeUntil(n int) []Volume {
+	res := make([]Volume, 0, n)
+
+	for i := 0; i < n; i++ {
+		res = append(res, Volume(i))
+	}
+
+	return res
+}
+
+func assertAllElementsUnique[T comparable](assertions *require.Assertions, list []T) {
+	m := make(map[T]bool, len(list))
+
+	for _, element := range list {
+		m[element] = true
+	}
+
+	assertions.Equal(len(list), len(m))
+}
+
+func TestVolumeBuilder_getBuilder(t *testing.T) {
+	t.Run(
+		"each_defined_var_has_unique_function", func(t *testing.T) {
+			assertions := require.New(t)
+
+			vb := &volumeBuilder{}
+
+			allBuilders := list.NewListMapTo[Volume, uintptr]().MapTo(
+				rangeUntil(numDefinedVolumes),
+				func(volume Volume) uintptr {
+					method := vb.getBuilder(volume)
+
+					return reflect.ValueOf(method).Pointer()
+				},
+			)
+
+			assertions.Len(allBuilders, numDefinedVolumes)
+			assertAllElementsUnique(assertions, allBuilders)
+		},
+	)
+
+	t.Run(
+		"panics_above_defined_limit", func(t *testing.T) {
+			assertions := require.New(t)
+
+			vb := &volumeBuilder{}
+
+			assertions.PanicsWithError(
+				"unknown volume requested", func() {
+					vb.getBuilder(numDefinedVolumes)
+				},
+			)
+		},
+	)
+}
+
+// TODO: Improvements here
+
+func TestVolumeBuilder_Build(t *testing.T) {
+	for _, test := range []struct {
+		name               string
+		isOpenShift        bool
+		expectedNumVolumes int
+	}{
+		{
+			name:               "isOpenShift",
+			isOpenShift:        true,
+			expectedNumVolumes: 8,
+		},
+		{
+			name:               "isNotOpenShift",
+			isOpenShift:        false,
+			expectedNumVolumes: 11,
+		},
+	} {
+		t.Run(
+			test.name, func(t *testing.T) {
+				assertions := require.New(t)
+
+				vb := NewVolumeBuilder(&instanav1.InstanaAgent{}, test.isOpenShift)
+
+				actual := vb.Build(rangeUntil(numDefinedVolumes)...)
+
+				assertions.Len(actual, test.expectedNumVolumes)
+			},
+		)
+	}
+}
