fd02e7cd3fdab0b3d3a193f68b7e8632c2e0f8ec
ZachRobinson <zachrobinson@ibm.com>
2024-02-19 14:17:30 -0600

    status.go tentative
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/k8s/operator/status/status.go
+++ pkg/k8s/operator/status/status.go
@@ -4,13 +4,18 @@ import (
 	"context"
 	"time"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	instanav1 "github.com/instana/instana-agent-operator/api/v1"
+	"github.com/instana/instana-agent-operator/pkg/result"
+
+	instanaclient "github.com/instana/instana-agent-operator/pkg/k8s/client"
 )
 
-func NewAgentStatusManager(k8sClient client.Client) AgentStatusManager {
+func NewAgentStatusManager(k8sClient instanaclient.InstanaAgentClient) AgentStatusManager {
 	return &agentStatusManager{
 		k8sClient:       k8sClient,
 		agentDaemonsets: make([]client.ObjectKey, 0, 1),
@@ -21,10 +26,11 @@ type AgentStatusManager interface {
 	AddAgentDaemonset(agentDaemonset client.ObjectKey)
 	SetK8sSensorDeployment(k8sSensorDeployment client.ObjectKey)
 	SetAgentConfigMap(agentConfigMap client.ObjectKey)
+	UpdateAgentStatus(ctx context.Context, reconcileErr error, agent client.ObjectKey) error
 }
 
 type agentStatusManager struct {
-	k8sClient client.Client
+	k8sClient instanaclient.InstanaAgentClient
 
 	agentDaemonsets     []client.ObjectKey
 	k8sSensorDeployment client.ObjectKey
@@ -61,16 +67,86 @@ func getReason(reconcileErr error) string {
 	}
 }
 
+func toResourceInfo(obj client.Object) result.Result[instanav1.ResourceInfo] {
+	return result.OfSuccess(
+		instanav1.ResourceInfo{
+			Name: obj.GetName(),
+			UID:  string(obj.GetUID()),
+		},
+	)
+}
+
+func (a *agentStatusManager) getDaemonSet(ctx context.Context) result.Result[instanav1.ResourceInfo] {
+	if len(a.agentDaemonsets) != 1 {
+		return result.OfSuccess(instanav1.ResourceInfo{})
+	}
+
+	ds := a.k8sClient.GetAsResult(ctx, a.agentDaemonsets[0], &appsv1.DaemonSet{})
+
+	return result.Map(ds, toResourceInfo)
+}
+
+func (a *agentStatusManager) getConfigMap(ctx context.Context) result.Result[instanav1.ResourceInfo] {
+	cm := a.k8sClient.GetAsResult(ctx, a.agentConfigMap, &corev1.ConfigMap{})
+
+	return result.Map(cm, toResourceInfo)
+}
+
 func (a *agentStatusManager) agentWithUpdatedStatus(
 	ctx context.Context,
 	reconcileErr error,
 	agentOld *instanav1.InstanaAgent,
-) *instanav1.InstanaAgent {
+) result.Result[*instanav1.InstanaAgent] {
 	agentNew := agentOld.DeepCopy()
 
 	agentNew.Status.Status = getAgentPhase(reconcileErr)
 	agentNew.Status.Reason = getReason(reconcileErr)
 	agentNew.Status.LastUpdate = metav1.Time{Time: time.Now()}
 
-	panic("TODO")
+	switch res := a.getDaemonSet(ctx); {
+	case res.IsSuccess():
+		ds, _ := res.Get()
+		agentNew.Status.DaemonSet = ds
+	case res.IsFailure():
+		_, err := res.Get()
+		return result.OfFailure[*instanav1.InstanaAgent](err)
+	}
+
+	switch res := a.getConfigMap(ctx); {
+	case res.IsSuccess():
+		cm, _ := res.Get()
+		agentNew.Status.ConfigMap = cm
+	case res.IsFailure():
+		_, err := res.Get()
+		return result.OfFailure[*instanav1.InstanaAgent](err)
+	}
+
+	return result.OfSuccess(agentNew)
+}
+
+func (a *agentStatusManager) UpdateAgentStatus(ctx context.Context, reconcileErr error, agent client.ObjectKey) error {
+	agentOld := &instanav1.InstanaAgent{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "instana.io/v1",
+			Kind:       "InstanaAgent",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      agent.Name,
+			Namespace: agent.Namespace,
+		},
+	}
+
+	switch res := a.agentWithUpdatedStatus(ctx, reconcileErr, agentOld); {
+	case res.IsSuccess():
+		agentNew, _ := res.Get()
+		return a.k8sClient.Status().Patch(
+			ctx,
+			agentNew,
+			client.MergeFrom(agentOld),
+			client.FieldOwner(instanaclient.FieldOwnerName),
+		)
+	default:
+		_, err := res.Get()
+		return err
+	}
 }
