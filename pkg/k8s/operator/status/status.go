86390ec1fcc1bf3d0629f0e96c1ae128fdaa63b9
ZachRobinson <zachrobinson@ibm.com>
2024-02-21 18:31:32 -0600

    all agents ready condition and ensure update attempt with all info
    Signed-off-by: ZachRobinson <zachrobinson@ibm.com>

--- pkg/k8s/operator/status/status.go
+++ pkg/k8s/operator/status/status.go
@@ -2,6 +2,7 @@ package status
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/Masterminds/semver/v3"
@@ -14,8 +15,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	instanav1 "github.com/instana/instana-agent-operator/api/v1"
+	"github.com/instana/instana-agent-operator/pkg/collections/list"
 	"github.com/instana/instana-agent-operator/pkg/env"
 	instanaclient "github.com/instana/instana-agent-operator/pkg/k8s/client"
+	"github.com/instana/instana-agent-operator/pkg/multierror"
 	"github.com/instana/instana-agent-operator/pkg/optional"
 	"github.com/instana/instana-agent-operator/pkg/result"
 )
@@ -160,10 +163,74 @@ func (a *agentStatusManager) getReconcileSucceededCondition(reconcileErr error) 
 	return res
 }
 
+func daemonsetIsAvailable(ds appsv1.DaemonSet) bool {
+	switch status := ds.Status; {
+	case ds.Generation != status.ObservedGeneration:
+		return false
+	case status.NumberMisscheduled != 0:
+		return false
+	case status.DesiredNumberScheduled != status.NumberAvailable:
+		return false
+	case status.DesiredNumberScheduled != status.UpdatedNumberScheduled:
+		return false
+	default:
+		return true
+	}
+}
+
+func (a *agentStatusManager) getAllAgentsReadyCondition(ctx context.Context) result.Result[metav1.Condition] {
+	condition := metav1.Condition{
+		Type:               ConditionTypeAllAgentsReady,
+		Status:             "",
+		ObservedGeneration: a.agentOld.GetGeneration(),
+		Reason:             "",
+		Message:            "",
+	}
+
+	dameonsets := make([]appsv1.DaemonSet, 0, len(a.agentDaemonsets))
+
+	for _, key := range a.agentDaemonsets {
+		var ds appsv1.DaemonSet
+		switch res := a.k8sClient.GetAsResult(ctx, key, &ds); {
+		case res.IsSuccess():
+			dameonsets = append(dameonsets, ds)
+		case res.IsFailure():
+			_, err := res.Get()
+
+			condition.Status = metav1.ConditionUnknown
+			condition.Reason = "AgentDaemonsetInfoUnavailable"
+			//goland:noinspection GoNilness
+			condition.Message = fmt.Sprintf(
+				"failed to retrieve status of Agent Daemonset: %s due to error: %s",
+				key.Name,
+				err.Error(),
+			)
+
+			return result.Of(condition, err)
+		}
+	}
+
+	// TODO: Implement a robust readiness endpoint in the agent for the readiness probe?
+	switch list.NewConditions(dameonsets).All(daemonsetIsAvailable) {
+	case true:
+		condition.Status = metav1.ConditionTrue
+		condition.Reason = "AllDesiredAgentsRunning"
+		condition.Message = "All desired Instana Agents are running and using up-to-date configuration"
+	default:
+		condition.Status = metav1.ConditionFalse
+		condition.Reason = "NotAllDesiredAgentsRunning"
+		condition.Message = "Not all desired Instana agents are running or some Agents are not using up-to-date configuration"
+	}
+
+	return result.OfSuccess(condition)
+}
+
 func (a *agentStatusManager) agentWithUpdatedStatus(
 	ctx context.Context,
 	reconcileErr error,
 ) result.Result[*instanav1.InstanaAgent] {
+	errBuilder := multierror.NewMultiErrorBuilder()
+
 	agentNew := a.agentOld.DeepCopy()
 	logger := log.FromContext(ctx).WithName("agent-status-manager")
 
@@ -173,23 +240,21 @@ func (a *agentStatusManager) agentWithUpdatedStatus(
 	agentNew.Status.Reason = getReason(reconcileErr)
 	agentNew.Status.LastUpdate = metav1.Time{Time: time.Now()}
 
-	switch res := a.getDaemonSet(ctx); {
-	case res.IsSuccess():
-		ds, _ := res.Get()
-		agentNew.Status.DaemonSet = ds
-	case res.IsFailure():
-		_, err := res.Get()
-		return result.OfFailure[*instanav1.InstanaAgent](err)
-	}
+	a.getDaemonSet(ctx).
+		OnSuccess(
+			func(ds instanav1.ResourceInfo) {
+				agentNew.Status.DaemonSet = ds
+			},
+		).
+		OnFailure(errBuilder.AddSingle)
 
-	switch res := a.getConfigMap(ctx); {
-	case res.IsSuccess():
-		cm, _ := res.Get()
-		agentNew.Status.ConfigMap = cm
-	case res.IsFailure():
-		_, err := res.Get()
-		return result.OfFailure[*instanav1.InstanaAgent](err)
-	}
+	a.getConfigMap(ctx).
+		OnSuccess(
+			func(cm instanav1.ResourceInfo) {
+				agentNew.Status.ConfigMap = cm
+			},
+		).
+		OnFailure(errBuilder.AddSingle)
 
 	// Handle New Status Fields
 
@@ -210,28 +275,31 @@ func (a *agentStatusManager) agentWithUpdatedStatus(
 					env.GetOperatorVersion(),
 				)
 			},
-		)
+		).
+		OnFailure(errBuilder.AddSingle)
 
 	// Handle Conditions
-
 	agentNew.Status.Conditions = optional.Of(agentNew.Status.Conditions).GetOrDefault(make([]metav1.Condition, 0, 3))
 	a.setConditionAndFireEvent(agentNew, a.getReconcileSucceededCondition(reconcileErr))
+	allAgentsReadyCondition, _ := a.getAllAgentsReadyCondition(ctx).OnFailure(errBuilder.AddSingle).Get()
+	a.setConditionAndFireEvent(agentNew, allAgentsReadyCondition)
 
-	return result.OfSuccess(agentNew)
+	return result.Of(agentNew, errBuilder.Build())
 }
 
 func (a *agentStatusManager) UpdateAgentStatus(ctx context.Context, reconcileErr error) error {
-	switch res := a.agentWithUpdatedStatus(ctx, reconcileErr); {
-	case res.IsSuccess():
-		agentNew, _ := res.Get()
-		return a.k8sClient.Status().Patch(
-			ctx,
-			agentNew,
-			client.MergeFrom(a.agentOld),
-			client.FieldOwner(instanaclient.FieldOwnerName),
-		)
-	default:
-		_, err := res.Get()
-		return err
+	errBuilder := multierror.NewMultiErrorBuilder()
+
+	agentNew, _ := a.agentWithUpdatedStatus(ctx, reconcileErr).OnFailure(errBuilder.AddSingle).Get()
+
+	if err := a.k8sClient.Status().Patch(
+		ctx,
+		agentNew,
+		client.MergeFrom(a.agentOld),
+		client.FieldOwner(instanaclient.FieldOwnerName),
+	); err != nil {
+		errBuilder.AddSingle(err)
 	}
+
+	return errBuilder.Build()
 }
