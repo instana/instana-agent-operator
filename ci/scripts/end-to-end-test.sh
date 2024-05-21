09a8a3c689a36dff442e734808984c668a1e42ee
Milica Cvrkota <milica.cvrkota@ibm.com>
2024-05-21 12:37:49 +0200

    wip:add new e2e base image for the operator

--- ci/scripts/end-to-end-test.sh
+++ ci/scripts/end-to-end-test.sh
@@ -11,4 +11,29 @@ set -o pipefail
 
 echo "running e2e tests"
 
+
+function get_public_image() {
+    git pull -r
+    VERSION=$(git tag | sort -r --version-sort | head -n1)
+    echo "Latest release is ${VERSION}"
+    export PREFIX="v"
+    export OPERATOR_DOCKER_VERSION=${VERSION#"$PREFIX"}
+
+    export IMG="icr.io/instana/instana-agent-operator:$OPERATOR_DOCKER_VERSION"
+    echo "IMG=$IMG"
+}
+
 source pipeline-source/ci/scripts/cluster-authentication.sh
+
+## deploy operator from main branch
+pushd pipeline-source
+    get_public_image
+
+    make install
+    # make deploy
+popd
+
+# check if the operator and agent pods are running
+
+## upgrade to the operator from the release branch
+# check if the operator and agent pods are running
