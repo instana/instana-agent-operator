e879e8d1819ceea24c9a8a48d510564053dd7a4e
Milica-Cvrkota-IBM <109787570+Milica-Cvrkota-IBM@users.noreply.github.com>
2024-07-02 12:00:15 +0200

    ci: Refactor creation of the public PRs (#166)
    Signed-off-by: Milica Cvrkota <milica.cvrkota@ibm.com>

---
+++ ci/scripts/commit-changes-to-public-repo.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#
+# (c) Copyright IBM Corp. 2024
+# (c) Copyright Instana Inc.
+#
+
+set -e
+set -o pipefail
+
+abort_if_pr_for_latest_version_exists() {
+    echo "Check if a PR is already open"
+    pr_list_json=$(curl -fH "Accept: application/vnd.github+json" https://api.github.com/repos/"$OWNER"/"$REPO"/pulls)
+    existing_pr_info_json=$(echo "$pr_list_json" | jq ".[] | select(.title == (\"$commit_message\"))")
+    if [ -n "$existing_pr_info_json" ]; then
+        echo "A PR is already open, exiting"
+        exit 0
+    fi
+    echo "PR does not exist, creating a PR"
+}
+
+pushd instana-agent-operator-release
+
+olm_bundle_zip=$(ls olm*.zip)
+operator_release_version="v$(echo "$olm_bundle_zip" | sed 's/olm-\(.*\)\.zip/\1/')"
+operator_public_pr_name="operator instana-agent-operator"
+if [ "$REPO" == "redhat-marketplace-operators" ]; then
+    operator_public_pr_name="${operator_public_pr_name}-rhmp"
+fi
+
+olm_bundle_zip_PATH="$(pwd)/$olm_bundle_zip"
+commit_message="$operator_public_pr_name ($operator_release_version)"
+
+abort_if_pr_for_latest_version_exists
+popd
+
+pushd "$PUBLIC_REPO_LOCAL_NAME"/operators/"$OPERATOR_NAME"
+
+echo "Creating a new feature branch from the upstream main"
+git remote add upstream https://github.com/"$OWNER"/"$REPO".git
+git fetch upstream
+git checkout main
+git reset --hard upstream/main
+git checkout -b "$OPERATOR_NAME-$operator_release_version"
+echo "Creation of the new PR branch was successful"
+
+echo "commit_message=$commit_message"
+set -x
+mkdir -p "$operator_release_version"
+unzip -o "$olm_bundle_zip_PATH" -d "$operator_release_version"
+
+if [ "$REPO" == "redhat-marketplace-operators" ]; then
+    pushd "$operator_release_version"
+
+    pushd manifests
+    yq -i '.metadata.annotations += {"marketplace.openshift.io/remote-workflow": "https://marketplace.redhat.com/en-us/operators/instana-agent-operator-rhmp/pricing?utm_source=openshift_console"}' instana-agent-operator.clusterserviceversion.yaml
+    yq -i '.metadata.annotations += {"marketplace.openshift.io/support-workflow": "https://marketplace.redhat.com/en-us/operators/instana-agent-operator-rhmp/support?utm_source=openshift_console"}' instana-agent-operator.clusterserviceversion.yaml
+    mv instana-agent-operator.clusterserviceversion.yaml instana-agent-operator-rhmp.clusterserviceversion.yaml
+    popd
+
+    pushd metadata
+    yq -i '.annotations."operators.operatorframework.io.bundle.package.v1" |= "instana-agent-operator-rhmp"' annotations.yaml
+    popd
+
+    popd
+fi
+
+
+git config --global user.name "instanacd"
+git config --global user.email "instanacd@instana.com"
+
+git add .
+git commit -s -m "$commit_message"
+
+popd
---
+++ ci/scripts/rebase-fork-branches.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#
+# (c) Copyright IBM Corp. 2024
+# (c) Copyright Instana Inc.
+#
+
+set -e
+set -o pipefail
+
+collect_pr_branches() {
+    echo "Collect all open PR branches"
+    pr_list_json=$(curl -fH "Accept: application/vnd.github+json" https://api.github.com/repos/"$OWNER"/"$REPO"/pulls)
+    pr_branches=$(echo "$pr_list_json" | jq -r ".[] | select(.title | contains(\"$operator_public_pr_name\")) | .head.ref")
+
+    if [ -z "$pr_branches" ]; then 
+        echo "No open PRs were found with the $operator_public_pr_name title"
+    else 
+        echo "Found the open PRs with the following branch names:"
+        echo "$pr_branches"
+    fi
+}
+
+rebase_branches() {
+    echo "Rebasing the forked main and the found PR branches from the upstream main branch"
+    pushd "$PUBLIC_REPO_LOCAL_NAME"
+    echo "Rebasing the forked main"
+    git remote add upstream https://github.com/"$OWNER"/"$REPO".git
+    git fetch upstream
+    git checkout main
+    git rebase upstream/main
+    git push --force
+    echo "Rebase of the forked main branch was successful"
+
+    echo "Rebasing the feature branches"
+    for branch in $pr_branches; do
+        echo "Rebasing PR branch $branch"
+        git checkout "$branch"
+        git rebase main
+        git push --force
+        echo "Rebase of PR branch $branch was successful"
+    done
+    popd
+}
+
+
+operator_public_pr_name="operator instana-agent-operator"
+if [ "$REPO" == "redhat-marketplace-operators" ]; then
+    operator_public_pr_name="${operator_public_pr_name}-rhmp"
+fi
+
+collect_pr_branches
+rebase_branches
