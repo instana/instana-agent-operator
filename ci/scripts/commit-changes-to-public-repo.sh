fea0ea9637e9b3284e56b643a05797181d2fb83f
Milica-Cvrkota-IBM <109787570+Milica-Cvrkota-IBM@users.noreply.github.com>
2024-07-16 15:42:17 +0200

    move helpers in a separate script (#197)
    Signed-off-by: Milica Cvrkota <milica.cvrkota@ibm.com>
    Signed-off-by: Rory Welch <Rory.Welch@ibm.com>

--- ci/scripts/commit-changes-to-public-repo.sh
+++ ci/scripts/commit-changes-to-public-repo.sh
@@ -8,16 +8,7 @@
 set -e
 set -o pipefail
 
-abort_if_pr_for_latest_version_exists() {
-    echo "Check if a PR is already open"
-    pr_list_json=$(curl -fH "Accept: application/vnd.github+json" https://api.github.com/repos/"$OWNER"/"$REPO"/pulls)
-    existing_pr_info_json=$(echo "$pr_list_json" | jq ".[] | select(.title == (\"$commit_message\"))")
-    if [ -n "$existing_pr_info_json" ]; then
-        echo "A PR is already open, exiting"
-        exit 0
-    fi
-    echo "PR does not exist, creating a PR"
-}
+source pipeline-source/ci/scripts/helpers.sh
 
 pushd instana-agent-operator-release
 
