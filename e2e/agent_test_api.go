4fe1a0373c7a9edef73e4aced22801697e98aa50
Mansoor Pervaiz <mansoor.pervaiz@ibm.com>
2024-11-15 07:13:30 -0500

    remove logging
    Signed-off-by: Mansoor Pervaiz <mansoor.pervaiz@ibm.com>

--- e2e/agent_test_api.go
+++ e2e/agent_test_api.go
@@ -330,14 +330,6 @@ func WaitForDeploymentToBecomeReady(name string) e2etypes.StepFunc {
 		// wait for operator pods of the deployment to become ready
 		err = wait.For(conditions.New(client.Resources()).DeploymentConditionMatch(&dep, appsv1.DeploymentAvailable, corev1.ConditionTrue), wait.WithTimeout(time.Minute*2))
 		if err != nil {
-			fmt.Println("")
-			fmt.Println("")
-			fmt.Println("")
-			fmt.Println("")
-			fmt.Println("")
-			fmt.Println(err)
-			fmt.Println("")
-			fmt.Println("")
 			t.Fatal(err)
 		}
 		t.Logf("Deployment %s is ready", name)
