b8e9bdd9edd8012c35a9176e7acff8da3c8df0ab
Milica Cvrkota <milica.cvrkota@ibm.com>
2025-01-15 18:54:00 +0100

    fix tests
    Signed-off-by: Milica Cvrkota <milica.cvrkota@ibm.com>

--- e2e/multizone_test.go
+++ e2e/multizone_test.go
@@ -7,12 +7,16 @@ package e2e
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"strings"
 	"testing"
 
 	"sigs.k8s.io/e2e-framework/klient/decoder"
 	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
+	"sigs.k8s.io/e2e-framework/support/utils"
 )
 
 func TestMultiZones(t *testing.T) {
@@ -25,10 +29,39 @@ func TestMultiZones(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			t.Logf("Creating dummy secret")
+			t.Log("Labeling the nodes")
+			nodeList := utils.FetchCommandOutput("kubectl get nodes -o name")
+			nodes := strings.Split(strings.TrimSpace(nodeList), "\n")
+			if len(nodes) < 2 {
+				log.Fatalf("Not enough nodes found for testing.")
+			}
+			node1 := strings.TrimPrefix(nodes[0], "node/")
+			node2 := strings.TrimPrefix(nodes[1], "node/")
 
-			t.Logf("Creating dummy agent CR with extra volume")
-			err = decoder.ApplyWithManifestDir(ctx, r, "../config/samples", "instana_v1_extended_instanaagent.yaml", []resources.CreateOption{})
+			p := utils.RunCommand(
+				fmt.Sprintf(
+					`kubectl label node %s %s`,
+					node1,
+					"pool=pool-01",
+				),
+			)
+			if p.Err() != nil {
+				t.Fatal("Error during labeling the nodes", p.Err(), p.Out(), p.ExitCode())
+			}
+
+			p = utils.RunCommand(
+				fmt.Sprintf(
+					`kubectl label nodes %s %s`,
+					node2,
+					"pool=pool-02",
+				),
+			)
+			if p.Err() != nil {
+				t.Fatal("Error during labeling the nodes", p.Err(), p.Out(), p.ExitCode())
+			}
+
+			t.Logf("Creating dummy agent CR with multizone enabled")
+			err = decoder.ApplyWithManifestDir(ctx, r, "../config/samples", "instana_v1_multizone_instanaagent.yaml", []resources.CreateOption{})
 			if err != nil {
 				t.Fatal(err)
 			}
