d2baf47fdd1f044833e3268e4f89e4a95a0ef543
Rory Welch <Rory.Welch@ibm.com>
2025-05-16 16:13:41 -0400

    Create finalizer.go for Remote Agent
    Signed-off-by: Rory Welch <Rory.Welch@ibm.com>

---
+++ controllers/remote_finalizer.go
@@ -0,0 +1,23 @@
+package controllers
+
+import (
+	"context"
+
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	v1 "github.com/instana/instana-agent-operator/api/v1"
+)
+
+func (r *RemoteAgentReconciler) addOrUpdateFinalizers(ctx context.Context, agentOld *v1.RemoteAgent) reconcileReturn {
+	agentNew := agentOld.DeepCopy()
+
+	switch removeHelmChartRes := r.cleanupHelmChart(ctx, agentNew); {
+	case removeHelmChartRes.suppliesReconcileResult():
+		return removeHelmChartRes
+	case controllerutil.AddFinalizer(agentNew, finalizerV3):
+		r.loggerFor(ctx, agentNew).V(1).Info("adding agent finalizer to agent CR")
+		return r.updateAgent(ctx, agentOld, agentNew)
+	default:
+		return reconcileContinue()
+	}
+}
