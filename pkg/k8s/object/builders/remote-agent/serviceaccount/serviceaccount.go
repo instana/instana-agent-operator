3a2c3a9a54c06256ad3490c92149931c40783bd3
Rory Welch <Rory.Welch@ibm.com>
2025-05-16 16:25:52 -0400

    Create Remote Agent Service Account
    Signed-off-by: Rory Welch <Rory.Welch@ibm.com>

---
+++ pkg/k8s/object/builders/remote-agent/serviceaccount/serviceaccount.go
@@ -0,0 +1,56 @@
+package serviceaccount
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	instanav1 "github.com/instana/instana-agent-operator/api/v1"
+	"github.com/instana/instana-agent-operator/pkg/k8s/object/builders/common/builder"
+	"github.com/instana/instana-agent-operator/pkg/k8s/object/builders/common/constants"
+	"github.com/instana/instana-agent-operator/pkg/k8s/object/builders/common/helpers"
+	"github.com/instana/instana-agent-operator/pkg/optional"
+	"github.com/instana/instana-agent-operator/pkg/pointer"
+)
+
+type serviceAccountBuilder struct {
+	*instanav1.RemoteAgent
+	helpers.RemoteHelpers
+}
+
+func (s *serviceAccountBuilder) IsNamespaced() bool {
+	return true
+}
+
+func (s *serviceAccountBuilder) ComponentName() string {
+	return constants.ComponentRemoteAgent
+}
+
+func (s *serviceAccountBuilder) build() *corev1.ServiceAccount {
+	return &corev1.ServiceAccount{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "ServiceAccount",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        s.ServiceAccountName(),
+			Namespace:   s.Namespace,
+			Annotations: s.Spec.ServiceAccountSpec.Annotations,
+		},
+	}
+}
+
+func (s *serviceAccountBuilder) Build() optional.Optional[client.Object] {
+	if pointer.DerefOrEmpty(s.Spec.ServiceAccountSpec.Create.Create) {
+		return optional.Of[client.Object](s.build())
+	} else {
+		return optional.Empty[client.Object]()
+	}
+}
+
+func NewServiceAccountBuilder(agent *instanav1.RemoteAgent) builder.ObjectBuilder {
+	return &serviceAccountBuilder{
+		RemoteAgent:   agent,
+		RemoteHelpers: helpers.NewRemoteHelpers(agent),
+	}
+}
