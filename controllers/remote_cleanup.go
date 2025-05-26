63bf4891b54fe864178812d88518c946bce30ebc
Rory Welch <Rory.Welch@ibm.com>
2025-05-26 14:18:01 -0400

    Remove unneeded code
    Signed-off-by: Rory Welch <Rory.Welch@ibm.com>

--- controllers/remote_cleanup.go
+++ controllers/remote_cleanup.go
@@ -19,35 +19,14 @@ package controllers
 
 import (
 	"context"
-	"errors"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/e2e-framework/support/utils"
 
 	instanav1 "github.com/instana/instana-agent-operator/api/v1"
-	"github.com/instana/instana-agent-operator/controllers/reconciliation/helm"
 	"github.com/instana/instana-agent-operator/pkg/k8s/operator/operator_utils"
 )
-
-func (r *RemoteAgentReconciler) cleanupHelmChart(
-	ctx context.Context,
-	agentOld *instanav1.RemoteAgent,
-) reconcileReturn {
-	agentNew := agentOld.DeepCopy()
-	log := r.loggerFor(ctx, agentNew)
-
-	if !controllerutil.RemoveFinalizer(agentNew, finalizerV1) {
-		log.V(2).Info("no deprecated Helm chart installation detected based on finalizers present on agent CR")
-		return reconcileContinue()
-	} else if err := helm.NewHelmReconciliation(r.scheme, log).Delete(); !errors.Is(err, nil) {
-		log.Error(err, "failed to uninstall deprecated Helm installation of remote agent")
-		return reconcileFailure(err)
-	} else {
-		log.V(1).Info("successfully uninstalled deprecated Helm installation of remote agent")
-		return r.updateAgent(ctx, agentOld, agentNew)
-	}
-}
 
 func (r *RemoteAgentReconciler) cleanupDependents(
 	ctx context.Context,
@@ -80,8 +59,6 @@ func (r *RemoteAgentReconciler) handleDeletion(
 	if agent.DeletionTimestamp == nil {
 		log.V(2).Info("agent is not under deletion")
 		return reconcileContinue()
-	} else if cleanupChartRes := r.cleanupHelmChart(ctx, agent); cleanupChartRes.suppliesReconcileResult() {
-		return cleanupChartRes
 	} else if cleanupDependentsRes := r.cleanupDependents(
 		ctx,
 		agent,
