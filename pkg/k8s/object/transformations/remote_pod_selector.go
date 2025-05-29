32c1967a4d6d2e982310226de459d5b1b7edca26
Rory Welch <Rory.Welch@ibm.com>
2025-05-29 10:20:37 -0400

    Use Remote Values
    Signed-off-by: Rory Welch <Rory.Welch@ibm.com>

--- pkg/k8s/object/transformations/remote_pod_selector.go
+++ pkg/k8s/object/transformations/remote_pod_selector.go
@@ -1,3 +1,18 @@
+/*
+(c) Copyright IBM Corp. 2025
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package transformations
 
 import (
@@ -20,7 +35,7 @@ type podSelectorLabelGeneratorRemote struct {
 func (p *podSelectorLabelGeneratorRemote) GetPodLabels(userLabels map[string]string) map[string]string {
 	podLabels := optional.Of(_map.NewCopier(userLabels).Copy()).GetOrDefault(make(map[string]string, 6))
 
-	podLabels[NameLabel] = name
+	podLabels[NameLabel] = nameRemote
 	podLabels[InstanceLabel] = p.Name
 	podLabels[ComponentLabel] = p.component
 	podLabels[PartOfLabel] = partOf
@@ -35,7 +50,7 @@ func (p *podSelectorLabelGeneratorRemote) GetPodLabels(userLabels map[string]str
 
 func (p *podSelectorLabelGeneratorRemote) GetPodSelectorLabels() map[string]string {
 	labels := map[string]string{
-		NameLabel:      name,
+		NameLabel:      nameRemote,
 		InstanceLabel:  p.Name,
 		ComponentLabel: p.component,
 	}
@@ -47,7 +62,7 @@ func (p *podSelectorLabelGeneratorRemote) GetPodSelectorLabels() map[string]stri
 	return labels
 }
 
-func PodSelectorLabelsRemote(agent *instanav1.RemoteAgent, component string) PodSelectorLabelGenerator {
+func PodSelectorLabelsRemote(agent *instanav1.RemoteAgent, component string) PodSelectorLabelGeneratorRemote {
 	return PodSelectorLabelsWithZoneInfoRemote(agent, component, nil)
 }
 
