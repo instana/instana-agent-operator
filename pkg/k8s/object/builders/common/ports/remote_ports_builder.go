c3802d47768715eb760b6d449d96796eb771095c
Rory Welch <Rory.Welch@ibm.com>
2025-05-29 10:11:08 -0400

    Update Licensing Headers
    Signed-off-by: Rory Welch <Rory.Welch@ibm.com>

--- pkg/k8s/object/builders/common/ports/remote_ports_builder.go
+++ pkg/k8s/object/builders/common/ports/remote_ports_builder.go
@@ -1,6 +1,5 @@
 /*
-(c) Copyright IBM Corp. 2024
-(c) Copyright Instana Inc.
+(c) Copyright IBM Corp. 2025
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
