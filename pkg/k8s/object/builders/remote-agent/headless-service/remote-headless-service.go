5eb1efa960cb2262a1c1b005e5137e5b6d2fd8dc
Rory Welch <Rory.Welch@ibm.com>
2025-05-29 10:18:00 -0400

    Use remote values
    Signed-off-by: Rory Welch <Rory.Welch@ibm.com>

--- pkg/k8s/object/builders/remote-agent/headless-service/remote-headless-service.go
+++ pkg/k8s/object/builders/remote-agent/headless-service/remote-headless-service.go
@@ -1,3 +1,18 @@
+/*
+(c) Copyright IBM Corp. 2025
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package headless_service
 
 import (
@@ -22,8 +37,8 @@ type remoteHeadlessServiceBuilder struct {
 	*instanav1.RemoteAgent
 
 	helpers.RemoteHelpers
-	transformations.PodSelectorLabelGenerator
-	ports.PortsBuilder
+	transformations.PodSelectorLabelGeneratorRemote
+	ports.PortsBuilderRemote
 }
 
 func (h *remoteHeadlessServiceBuilder) Build() builder.OptionalObject {
@@ -42,9 +57,6 @@ func (h *remoteHeadlessServiceBuilder) Build() builder.OptionalObject {
 				Selector:  h.GetPodSelectorLabels(),
 				Ports: h.GetServicePorts(
 					ports.AgentAPIsPort,
-					ports.OpenTelemetryLegacyPort,
-					ports.OpenTelemetryGRPCPort,
-					ports.OpenTelemetryHTTPPort,
 				),
 			},
 		},
@@ -61,10 +73,9 @@ func (h *remoteHeadlessServiceBuilder) IsNamespaced() bool {
 
 func NewHeadlessServiceBuilder(agent *instanav1.RemoteAgent) builder.ObjectBuilder {
 	return &remoteHeadlessServiceBuilder{
-		RemoteAgent: agent,
-
-		RemoteHelpers:             helpers.NewRemoteHelpers(agent),
-		PodSelectorLabelGenerator: transformations.PodSelectorLabelsRemote(agent, componentName),
-		PortsBuilder:              ports.NewPortsBuilderRemote(agent),
+		RemoteAgent:                     agent,
+		RemoteHelpers:                   helpers.NewRemoteHelpers(agent),
+		PodSelectorLabelGeneratorRemote: transformations.PodSelectorLabelsRemote(agent, componentName),
+		PortsBuilderRemote:              ports.NewPortsBuilderRemote(agent),
 	}
 }
