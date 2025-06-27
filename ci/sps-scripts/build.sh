f113a9dba4dfeceec9bf61cc29e400383bb4c801
nagarajkandoor <nagaraj.kandoor@ibm.com>
2025-06-27 15:52:44 +0200

    INSTA-12464-added-olm-build

--- ci/sps-scripts/build.sh
+++ ci/sps-scripts/build.sh
@@ -8,21 +8,21 @@ echo "Building instana-agent helm chart..."
 
 SOURCE_DIRECTORY=$(git rev-parse --show-toplevel)
 TARGET_DIRECTORY=artefacts
-NEW_CHART_VERSION=$(cat "${SOURCE_DIRECTORY}/versions/INSTANA_AGENT_CHART_VERSION")
-HELM_APP_VERSION=$(cat "${SOURCE_DIRECTORY}/versions/INSTANA_AGENT_APP_VERSION")
+NEW_CHART_VERSION=$(cat "$WORKSPACE/$APP_REPO_FOLDER/versions/INSTANA_AGENT_CHART_VERSION")
+HELM_APP_VERSION=$(cat "$WORKSPACE/$APP_REPO_FOLDER/versions/INSTANA_AGENT_APP_VERSION")
 
-cd "${SOURCE_DIRECTORY}"
+cd "$WORKSPACE/$APP_REPO_FOLDER/"
 rm -rf "${TARGET_DIRECTORY}"
 mkdir -p "${TARGET_DIRECTORY}"
 
 CHART_TARGET_DIRECTORY="${TARGET_DIRECTORY}/helm-charts"
 
 echo "Copying changes from canonical to ${CHART_TARGET_DIRECTORY}"
-rsync -avr "${SOURCE_DIRECTORY}/canonical/." "${CHART_TARGET_DIRECTORY}"
+rsync -avr "$WORKSPACE/$APP_REPO_FOLDER/canonical/." "${CHART_TARGET_DIRECTORY}"
 
 echo "Injecting versions in Chart.yaml"
-yq eval -i ".appVersion = \"${HELM_APP_VERSION}\"" "${SOURCE_DIRECTORY}/${CHART_TARGET_DIRECTORY}/Chart.yaml"
-yq eval -i ".version = \"${NEW_CHART_VERSION}\"" "${SOURCE_DIRECTORY}/${CHART_TARGET_DIRECTORY}/Chart.yaml"
+yq eval -i ".appVersion = \"${HELM_APP_VERSION}\"" "$WORKSPACE/$APP_REPO_FOLDER/${CHART_TARGET_DIRECTORY}/Chart.yaml"
+yq eval -i ".version = \"${NEW_CHART_VERSION}\"" "$WORKSPACE/$APP_REPO_FOLDER/${CHART_TARGET_DIRECTORY}/Chart.yaml"
 
 
 echo "Downloading operator release yaml"
@@ -31,12 +31,12 @@ mkdir -p operator-download
 pushd operator-download
 curl -L https://github.com/instana/instana-agent-operator/releases/latest/download/instana-agent-operator.yaml | yq  -s '"operator_" + .kind + "_" + .metadata.name'
 echo "Extracting CRD"
-mkdir -p "${SOURCE_DIRECTORY}/${CHART_TARGET_DIRECTORY}/crds"
+mkdir -p "$WORKSPACE/$APP_REPO_FOLDER/${CHART_TARGET_DIRECTORY}/crds"
 # ensure to use lowercase letters for all filenames
 for file in *; do
   mv "$file" "$(echo "$file" | tr '[:upper:]' '[:lower:]')"
 done
-mv ./*customresourcedefinition* "${SOURCE_DIRECTORY}/${CHART_TARGET_DIRECTORY}/crds/operator_customresourcedefinition_agents_instana_io.yml"
+mv ./*customresourcedefinition* "$WORKSPACE/$APP_REPO_FOLDER/${CHART_TARGET_DIRECTORY}/crds/operator_customresourcedefinition_agents_instana_io.yml"
 
 # fetch current value of container image from the operator release and use it for default values.yaml
 OPERARTOR_IMAGE=$(yq ".spec.template.spec.containers[0].image" "./operator_deployment_instana-agent-controller-manager.yml")
@@ -45,9 +45,9 @@ OPERARTOR_REPO=$(echo "${OPERARTOR_IMAGE}" | awk -F '[:]' '{print $1}')
 echo "OPERARTOR_REPO=${OPERARTOR_REPO}"
 OPERARTOR_TAG=$(echo "${OPERARTOR_IMAGE}" | awk -F '[:]' '{print $2}')
 echo "OPERARTOR_TAG=${OPERARTOR_TAG}"
-yq eval -i ".controllerManager.image.name = \"${OPERARTOR_REPO}\"" "${SOURCE_DIRECTORY}/${CHART_TARGET_DIRECTORY}/values.yaml"
-yq eval -i ".controllerManager.image.tag = \"${OPERARTOR_TAG}\"" "${SOURCE_DIRECTORY}/${CHART_TARGET_DIRECTORY}/values.yaml"
-sed -i "s/instana-agent-operator:latest/instana-agent-operator:${OPERARTOR_TAG}/g" "${SOURCE_DIRECTORY}/${CHART_TARGET_DIRECTORY}/templates/operator_deployment_instana-agent-controller-manager.yml"
+yq eval -i ".controllerManager.image.name = \"${OPERARTOR_REPO}\"" "$WORKSPACE/$APP_REPO_FOLDER/${CHART_TARGET_DIRECTORY}/values.yaml"
+yq eval -i ".controllerManager.image.tag = \"${OPERARTOR_TAG}\"" "$WORKSPACE/$APP_REPO_FOLDER/${CHART_TARGET_DIRECTORY}/values.yaml"
+sed -i "s/instana-agent-operator:latest/instana-agent-operator:${OPERARTOR_TAG}/g" "$WORKSPACE/$APP_REPO_FOLDER/${CHART_TARGET_DIRECTORY}/templates/operator_deployment_instana-agent-controller-manager.yml"
 
 popd
 rm -rf operator-download
