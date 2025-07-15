eaf7f901827fc22c50d071c54b8fbf97ff50d9df
Rory Welch <Rory.Welch@ibm.com>
2025-07-14 21:57:36 -0400

    Get existing remote agent for test
    Signed-off-by: Rory Welch <Rory.Welch@ibm.com>

--- e2e/agent_remote_test_api.go
+++ e2e/agent_remote_test_api.go
@@ -140,6 +140,14 @@ func EnsureAgentRemoteDeletion() env.Func {
 			return ctx, fmt.Errorf("failed to initialize client: %v", err)
 		}
 
+		agent := &v1.InstanaAgentRemote{}
+		err = r.Get(ctx, AgentRemoteCustomResourceName, InstanaNamespace, agent)
+		if errors.IsNotFound(err) {
+			// No agent cr found, skip this cleanup step
+			log.Info("No agent remote CR present, skipping deletion")
+			return ctx, nil
+		}
+
 		p := utils.RunCommand("kubectl get pods -n instana-agent")
 		log.Info("Current pods: ", p.Command(), p.ExitCode(), "\n", p.Result())
 
@@ -163,7 +171,6 @@ func EnsureAgentRemoteDeletion() env.Func {
 		// full purge of remote resources if anything would be left in the cluster
 		// Removing the finalizer from the existing Agent CR to make it deletable
 		// kubectl patch agent instana-agent-remote -p '{"metadata":{"finalizers":[]}}' --type=merge
-		agent := &v1.InstanaAgentRemote{}
 		log.Info("Patching agent remote cr to remove finalizers")
 		err = r.Patch(ctx, agent, k8s.Patch{
 			PatchType: types.MergePatchType,
