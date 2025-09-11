0686a025030e2385c1f670a94610d071231ccaca
Konrad Ohms <konrad.ohms@de.ibm.com>
2025-09-11 17:06:53 +0200

    test: add tests for secret mount functionality in keys-secret
    Added comprehensive test cases for the new secret mount functionality:
    - Test basic secret mounts with agent key and download key
    - Test proxy credentials handling
    - Test mirror repository credentials handling
    - Test HTTPS_PROXY generation with different configurations
    - Test multiple backends handling
    - Test behavior when UseSecretMounts is false
    
    All tests verify that when UseSecretMounts is true, the secret data includes
    the additional entries needed for file mounting.
    
    Signed-off-by: Konrad Ohms <konrad.ohms@de.ibm.com>

--- pkg/k8s/object/builders/agent/secrets/keys-secret/secret_test.go
+++ pkg/k8s/object/builders/agent/secrets/keys-secret/secret_test.go
@@ -1,5 +1,5 @@
 /*
-(c) Copyright IBM Corp. 2024
+(c) Copyright IBM Corp. 2024, 2025
 (c) Copyright Instana Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,6 +29,7 @@ import (
 	instanav1 "github.com/instana/instana-agent-operator/api/v1"
 	backends "github.com/instana/instana-agent-operator/pkg/k8s/object/builders/common/backends"
 	"github.com/instana/instana-agent-operator/pkg/optional"
+	"github.com/instana/instana-agent-operator/pkg/pointer"
 )
 
 func TestSecretBuilder_IsNamespaced_ComponentName(t *testing.T) {
@@ -121,5 +122,376 @@ func TestSecretBuilder_Build(t *testing.T) {
 				)
 			}
 		}
+	}
+}
+
+func TestSecretBuilder_BuildWithSecretMounts(t *testing.T) {
+	testCases := []struct {
+		name           string
+		agent          *instanav1.InstanaAgent
+		backends       []backends.K8SensorBackend
+		expectedSecret *corev1.Secret
+	}{
+		{
+			name: "with UseSecretMounts true and agent key and download key",
+			agent: &instanav1.InstanaAgent{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Spec: instanav1.InstanaAgentSpec{
+					UseSecretMounts: pointer.To(true),
+					Agent: instanav1.BaseAgentSpec{
+						Key:         "test-key",
+						DownloadKey: "test-download-key",
+					},
+				},
+			},
+			backends: []backends.K8SensorBackend{
+				{
+					ResourceSuffix: "",
+					EndpointKey:    "test-key",
+				},
+			},
+			expectedSecret: &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Secret",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Data: map[string][]byte{
+					"key":                  []byte("test-key"),
+					"downloadKey":          []byte("test-download-key"),
+					"INSTANA_AGENT_KEY":    []byte("test-key"),
+					"INSTANA_DOWNLOAD_KEY": []byte("test-download-key"),
+				},
+				Type: corev1.SecretTypeOpaque,
+			},
+		},
+		{
+			name: "with UseSecretMounts true and proxy credentials",
+			agent: &instanav1.InstanaAgent{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Spec: instanav1.InstanaAgentSpec{
+					UseSecretMounts: pointer.To(true),
+					Agent: instanav1.BaseAgentSpec{
+						Key:           "test-key",
+						DownloadKey:   "test-download-key",
+						ProxyUser:     "proxy-user",
+						ProxyPassword: "proxy-password",
+					},
+				},
+			},
+			backends: []backends.K8SensorBackend{
+				{
+					ResourceSuffix: "",
+					EndpointKey:    "test-key",
+				},
+			},
+			expectedSecret: &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Secret",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Data: map[string][]byte{
+					"key":                          []byte("test-key"),
+					"downloadKey":                  []byte("test-download-key"),
+					"INSTANA_AGENT_KEY":            []byte("test-key"),
+					"INSTANA_DOWNLOAD_KEY":         []byte("test-download-key"),
+					"INSTANA_AGENT_PROXY_USER":     []byte("proxy-user"),
+					"INSTANA_AGENT_PROXY_PASSWORD": []byte("proxy-password"),
+				},
+				Type: corev1.SecretTypeOpaque,
+			},
+		},
+		{
+			name: "with UseSecretMounts true and mirror repository credentials",
+			agent: &instanav1.InstanaAgent{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Spec: instanav1.InstanaAgentSpec{
+					UseSecretMounts: pointer.To(true),
+					Agent: instanav1.BaseAgentSpec{
+						Key:                       "test-key",
+						DownloadKey:               "test-download-key",
+						MirrorReleaseRepoUsername: "release-user",
+						MirrorReleaseRepoPassword: "release-password",
+						MirrorSharedRepoUsername:  "shared-user",
+						MirrorSharedRepoPassword:  "shared-password",
+					},
+				},
+			},
+			backends: []backends.K8SensorBackend{
+				{
+					ResourceSuffix: "",
+					EndpointKey:    "test-key",
+				},
+			},
+			expectedSecret: &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Secret",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Data: map[string][]byte{
+					"key":                  []byte("test-key"),
+					"downloadKey":          []byte("test-download-key"),
+					"INSTANA_AGENT_KEY":    []byte("test-key"),
+					"INSTANA_DOWNLOAD_KEY": []byte("test-download-key"),
+					"AGENT_RELEASE_REPOSITORY_MIRROR_USERNAME":  []byte("release-user"),
+					"AGENT_RELEASE_REPOSITORY_MIRROR_PASSWORD":  []byte("release-password"),
+					"INSTANA_SHARED_REPOSITORY_MIRROR_USERNAME": []byte("shared-user"),
+					"INSTANA_SHARED_REPOSITORY_MIRROR_PASSWORD": []byte("shared-password"),
+				},
+				Type: corev1.SecretTypeOpaque,
+			},
+		},
+		{
+			name: "with UseSecretMounts true and HTTPS_PROXY with host only",
+			agent: &instanav1.InstanaAgent{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Spec: instanav1.InstanaAgentSpec{
+					UseSecretMounts: pointer.To(true),
+					Agent: instanav1.BaseAgentSpec{
+						Key:         "test-key",
+						DownloadKey: "test-download-key",
+						ProxyHost:   "proxy.example.com",
+					},
+				},
+			},
+			backends: []backends.K8SensorBackend{
+				{
+					ResourceSuffix: "",
+					EndpointKey:    "test-key",
+				},
+			},
+			expectedSecret: &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Secret",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Data: map[string][]byte{
+					"key":                  []byte("test-key"),
+					"downloadKey":          []byte("test-download-key"),
+					"INSTANA_AGENT_KEY":    []byte("test-key"),
+					"INSTANA_DOWNLOAD_KEY": []byte("test-download-key"),
+					"HTTPS_PROXY":          []byte("http://proxy.example.com:80"),
+				},
+				Type: corev1.SecretTypeOpaque,
+			},
+		},
+		{
+			name: "with UseSecretMounts true and HTTPS_PROXY with host, port, protocol",
+			agent: &instanav1.InstanaAgent{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Spec: instanav1.InstanaAgentSpec{
+					UseSecretMounts: pointer.To(true),
+					Agent: instanav1.BaseAgentSpec{
+						Key:           "test-key",
+						DownloadKey:   "test-download-key",
+						ProxyHost:     "proxy.example.com",
+						ProxyPort:     "8080",
+						ProxyProtocol: "https",
+					},
+				},
+			},
+			backends: []backends.K8SensorBackend{
+				{
+					ResourceSuffix: "",
+					EndpointKey:    "test-key",
+				},
+			},
+			expectedSecret: &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Secret",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Data: map[string][]byte{
+					"key":                  []byte("test-key"),
+					"downloadKey":          []byte("test-download-key"),
+					"INSTANA_AGENT_KEY":    []byte("test-key"),
+					"INSTANA_DOWNLOAD_KEY": []byte("test-download-key"),
+					"HTTPS_PROXY":          []byte("https://proxy.example.com:8080"),
+				},
+				Type: corev1.SecretTypeOpaque,
+			},
+		},
+		{
+			name: "with UseSecretMounts true and HTTPS_PROXY with credentials",
+			agent: &instanav1.InstanaAgent{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Spec: instanav1.InstanaAgentSpec{
+					UseSecretMounts: pointer.To(true),
+					Agent: instanav1.BaseAgentSpec{
+						Key:           "test-key",
+						DownloadKey:   "test-download-key",
+						ProxyHost:     "proxy.example.com",
+						ProxyPort:     "8080",
+						ProxyProtocol: "https",
+						ProxyUser:     "proxy-user",
+						ProxyPassword: "proxy-password",
+					},
+				},
+			},
+			backends: []backends.K8SensorBackend{
+				{
+					ResourceSuffix: "",
+					EndpointKey:    "test-key",
+				},
+			},
+			expectedSecret: &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Secret",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Data: map[string][]byte{
+					"key":                          []byte("test-key"),
+					"downloadKey":                  []byte("test-download-key"),
+					"INSTANA_AGENT_KEY":            []byte("test-key"),
+					"INSTANA_DOWNLOAD_KEY":         []byte("test-download-key"),
+					"INSTANA_AGENT_PROXY_USER":     []byte("proxy-user"),
+					"INSTANA_AGENT_PROXY_PASSWORD": []byte("proxy-password"),
+					"HTTPS_PROXY": []byte(
+						"https://proxy-user:proxy-password@proxy.example.com:8080",
+					),
+				},
+				Type: corev1.SecretTypeOpaque,
+			},
+		},
+		{
+			name: "with UseSecretMounts true and multiple backends",
+			agent: &instanav1.InstanaAgent{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Spec: instanav1.InstanaAgentSpec{
+					UseSecretMounts: pointer.To(true),
+					Agent: instanav1.BaseAgentSpec{
+						Key:         "test-key",
+						DownloadKey: "test-download-key",
+					},
+				},
+			},
+			backends: []backends.K8SensorBackend{
+				{
+					ResourceSuffix: "",
+					EndpointKey:    "first-backend-key",
+				},
+				{
+					ResourceSuffix: "-2",
+					EndpointKey:    "second-backend-key",
+				},
+			},
+			expectedSecret: &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Secret",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Data: map[string][]byte{
+					"key":                  []byte("first-backend-key"),
+					"key-2":                []byte("second-backend-key"),
+					"downloadKey":          []byte("test-download-key"),
+					"INSTANA_AGENT_KEY":    []byte("first-backend-key"),
+					"INSTANA_DOWNLOAD_KEY": []byte("test-download-key"),
+				},
+				Type: corev1.SecretTypeOpaque,
+			},
+		},
+		{
+			name: "with UseSecretMounts false",
+			agent: &instanav1.InstanaAgent{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Spec: instanav1.InstanaAgentSpec{
+					UseSecretMounts: pointer.To(false),
+					Agent: instanav1.BaseAgentSpec{
+						Key:           "test-key",
+						DownloadKey:   "test-download-key",
+						ProxyUser:     "proxy-user",
+						ProxyPassword: "proxy-password",
+					},
+				},
+			},
+			backends: []backends.K8SensorBackend{
+				{
+					ResourceSuffix: "",
+					EndpointKey:    "test-key",
+				},
+			},
+			expectedSecret: &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Secret",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-agent",
+					Namespace: "test-namespace",
+				},
+				Data: map[string][]byte{
+					"key":         []byte("test-key"),
+					"downloadKey": []byte("test-download-key"),
+				},
+				Type: corev1.SecretTypeOpaque,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assertions := require.New(t)
+
+			sb := NewSecretBuilder(tc.agent, tc.backends)
+			actual := sb.Build()
+
+			if tc.expectedSecret == nil {
+				assertions.Empty(actual)
+			} else {
+				expected := optional.Of[client.Object](tc.expectedSecret)
+				assertions.Equal(expected, actual)
+			}
+		})
 	}
 }
