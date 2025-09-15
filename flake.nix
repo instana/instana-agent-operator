26d4803482ee78b8974beb6f927b95bc3d5afbb3
Antonio Gutierrez <antonio.gutierrez1@ibm.com>
2025-09-15 19:30:47 +0200

    flake.nix: reformat flake and add $PWD/bin to PATH to use controller-gen
    Signed-off-by: Antonio Gutierrez <antonio.gutierrez1@ibm.com>

--- flake.nix
+++ flake.nix
@@ -1,53 +1,65 @@
 {
-  description =
-    "A nix-flake-based Go/Kubernetes Controller development environment using operator-sdk";
+  description = "A nix-flake-based Go/Kubernetes Controller development environment using operator-sdk";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       regex = "go[[:space:]]+([0-9]+\\.[0-9]+)(\\.[0-9]+)?";
       # Read and get all the lines of the go.mod files and filter the empty []
-      lines = builtins.filter (line: line != [ ])
-        (builtins.split "\n" (builtins.readFile ./go.mod));
+      lines = builtins.filter (line: line != [ ]) (builtins.split "\n" (builtins.readFile ./go.mod));
       # Get the line that has the `go xx.xx.xx` or `go xx.xx`
-      matchingLines =
-        builtins.filter (line: builtins.match regex line != null) lines;
+      matchingLines = builtins.filter (line: builtins.match regex line != null) lines;
       # Get the version string i.e: "1.23"
-      versionString = builtins.elemAt
-        (builtins.match regex (builtins.concatStringsSep " " matchingLines)) 0;
+      versionString = builtins.elemAt (builtins.match regex (builtins.concatStringsSep " " matchingLines)) 0;
       # Get the string as "1_23"
       versionForOverlay = builtins.replaceStrings [ "." ] [ "_" ] versionString;
 
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
           f {
             pkgs = import nixpkgs {
               inherit system;
               overlays = [ self.overlays.default ];
             };
-          });
-    in {
+          }
+        );
+    in
+    {
       overlays.default = final: prev: { go = final."go_${versionForOverlay}"; };
 
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            # go (version is specified by overlay)
-            go
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # go (version is specified by overlay)
+              go
 
-            # gopls (go language server)
-            gopls
+              # gopls (go language server)
+              gopls
 
-            # goimports, godoc, etc.
-            gotools
+              # goimports, godoc, etc.
+              gotools
 
-            # SDK for building Kubernetes applications
-            operator-sdk
-          ];
-        };
-      });
+              # SDK for building Kubernetes applications
+              operator-sdk
+            ];
+            # Adds to PATH variable the path of where the controller-gen is downloaded to
+            shellHook = ''
+              export PATH=$PATH:$PWD/bin
+            '';
+          };
+        }
+      );
     };
 }
