c91730f99636ab84daebd282e565a15293c80f96
Konrad Ohms <konrad.ohms@de.ibm.com>
2025-10-06 08:53:49 +0200

    fix: ignore status patch errors after agent deletion
    Treat NotFound responses from the status patch as a normal part of finalizer teardown so controller-runtime stops warning about result/error mismatches.
    
    Signed-off-by: Konrad Ohms <konrad.ohms@de.ibm.com>

--- pkg/k8s/operator/status/agent_status_manager.go
+++ pkg/k8s/operator/status/agent_status_manager.go
@@ -11,6 +11,7 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -98,7 +99,7 @@ func (a *agentStatusManager) UpdateAgentStatus(ctx context.Context, reconcileErr
 		agentNew,
 		client.MergeFrom(a.agentOld),
 		client.FieldOwner(instanaclient.FieldOwnerName),
-	); err != nil {
+	); err != nil && !k8serrors.IsNotFound(err) {
 		errBuilder.AddSingle(err)
 	}
 
