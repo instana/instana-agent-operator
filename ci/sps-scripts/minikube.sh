2566dbc70d2bf7bed256c7441d74409907c853f5
Konrad Ohms <konrad.ohms@de.ibm.com>
2025-10-10 12:15:30 +0200

    ci: try kind

--- ci/sps-scripts/minikube.sh
+++ ci/sps-scripts/minikube.sh
@@ -11,27 +11,39 @@ if [[ "$PIPELINE_DEBUG" == 1 ]]; then
 fi
 echo "===== minikube.sh - start ====="
 
+# do not fail, just try things
+set +e
 mkdir -p bin
 
-curl -LO \
-  https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
-  && install minikube-linux-amd64 bin/
-  
 kv=$(curl -sSL https://dl.k8s.io/release/stable.txt)
 curl -LO \
   https://dl.k8s.io/$kv/bin/linux/amd64/kubectl \
   && install kubectl bin/
 
 PATH=$PATH:$(pwd)/bin
-echo "DOCKER_HOST=$DOCKER_HOST"
 export DOCKER_HOST=tcp://localhost:2376
-echo "setting DOCKER_HOST=tcp://localhost:2376 explicitly"
+export DOCKER_TLS_VERIFY=1
+export DOCKER_CERT_PATH=/certs/client
+echo "setting DOCKER_HOST=tcp://localhost:2375 explicitly"
 
+curl -LO \
+  https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && install minikube-linux-amd64 bin/
 minikube-linux-amd64 config set WantUpdateNotification false
 minikube-linux-amd64 start --driver=docker --force
+
+# Install kind
+# curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
+# chmod +x ./kind
+# mv ./kind bin/kind
+
+# # Create cluster
+# kind create cluster --wait 5m
 
 kubectl get nodes -o wide
 kubectl get pods
 kubectl get ns
+
+docker ps
 
 echo "===== minikube.sh - stop ====="
