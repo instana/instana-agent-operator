4f946ed156235e031b1fe555e402bd7eb90c258a
Konrad Ohms <konrad.ohms@de.ibm.com>
2025-10-10 15:55:12 +0200

    ci: try setting flags on minikube cli

--- ci/sps-scripts/minikube.sh
+++ ci/sps-scripts/minikube.sh
@@ -30,7 +30,13 @@ curl -LO \
   https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && install minikube-linux-amd64 bin/
 minikube-linux-amd64 config set WantUpdateNotification false
-minikube-linux-amd64 start --driver=docker --force
+minikube-linux-amd64 start \
+  --driver=docker \
+  --force \
+  --container-runtime=docker \
+  --extra-config=kubelet.cgroup-driver=cgroupfs \
+  --wait=all \
+  --wait-timeout=10m
 
 # Install kind
 # curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
